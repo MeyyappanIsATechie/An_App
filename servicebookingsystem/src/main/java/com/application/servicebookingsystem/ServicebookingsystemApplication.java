8057eed95f6e7609edc4b0a46f78ade5bb13e972
Meyyappan <meyyappan721@gmail.com>
2024-09-22 17:53:25 +0530

    initial commit

---
+++ servicebookingsystem/src/main/java/com/application/servicebookingsystem/ServicebookingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.application.servicebookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicebookingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicebookingsystemApplication.class, args);
+	}
+
+}
---
+++ servicebookingsystem/src/test/java/com/application/servicebookingsystem/ServicebookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.servicebookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicebookingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
